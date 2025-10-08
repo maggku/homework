c7a1d7fd00786509d54eda95989d3abe855137a4
maggieku <maggie.kuczynska@gmal.com>
2025-10-08 13:42:35 +0100

    Decorator homework - profiling function in decorator - decorator2.py

--- decorator2.py
+++ decorator2.py
@@ -1,9 +1,13 @@
+import time
 from decorator1 import main, larger
 
 def decorator(func):
     def wrapper(text):
+        start = time.time()
         print("Hello this is a decorator", end = " for ")
         func(text)
+        end = time.time()
+        print(f"This function tookq {end - start} seconds")
     return wrapper
 
 @decorator
