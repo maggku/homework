fc7fafea1ed9a0d4557d6d06ae4cd1a6ceb28976
maggieku <maggie.kuczynska@gmal.com>
2025-10-08 15:42:13 +0100

    New branch feature3 first commit, homework assignment

---
+++ except.py
@@ -0,0 +1,16 @@
+#Napisz funkcję, która dzieli dwie liczby, ale zabezpiecza się przed błędami za pomocą try/except.
+#Powinna użyć try/except, aby:
+#
+#   złapać ZeroDivisionError (dzielenie przez 0) i zwrócić tekst "Nie można dzielić przez zero!",
+#  złapać TypeError (np. gdy argumenty nie są liczbami) i zwrócić tekst "Podano nieprawidłowe dane!".
+
+# W przypadku sukcesu zwrócić wynik dzielenia.
+#
+#Napisz proszę testy do tej f-cji:
+
+#    Czy dzielenie 10, 2 daje 5
+#    Obsługa dzielenia przez 0.
+#    Obsługa przy podaniu nieprawidłowych danych.
+
+#zadanie na szóstkę:
+#Do testów wykorzystaj moduł "pytest". Trzeba będgit zie go zainstalować.
